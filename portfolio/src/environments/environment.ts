351cf71bf4fe952516ee86022ac12453056f6b93
RamkumarMrj <ramkumar.mrj97@gmail.com>
2024-05-18 17:49:30 +0530

    data update

--- portfolio/src/environments/environment.ts
+++ portfolio/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://gist.githubusercontent.com/RamkumarMrj/aef0357b69bd4cb6bd25b8875a815e52/raw/b4d894806075d77cce811dd2cd21829ea092fe94/resume.json'
+    baseUrl: 'https://gist.githubusercontent.com/RamkumarMrj/aef0357b69bd4cb6bd25b8875a815e52/raw/6d318616abd2998983f3ab5d60ff3bf13557674e/resume.json'
 };
