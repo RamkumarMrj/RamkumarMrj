dee9030306224d1b277938c2c6524451d50cce23
RamkumarMrj <ramkumar.mrj97@gmail.com>
2026-03-27 23:04:06 +0530

    Migrate to Angular 18

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -11,32 +11,26 @@ import { SkillsComponent } from './components/background/skills/skills.component
 import { CertificationsComponent } from './components/background/certifications/certifications.component';
 import { EducationComponent } from './components/background/education/education.component';
 import { ReferencesComponent } from './components/background/references/references.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { APP_BASE_HREF } from '@angular/common';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    BackgroundComponent,
-    WorkExperienceComponent,
-    ProjectsComponent,
-    SkillsComponent,
-    CertificationsComponent,
-    EducationComponent,
-    ReferencesComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatIconModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideClientHydration(),
-    { provide: APP_BASE_HREF, useValue: '/' }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ProfileComponent,
+        BackgroundComponent,
+        WorkExperienceComponent,
+        ProjectsComponent,
+        SkillsComponent,
+        CertificationsComponent,
+        EducationComponent,
+        ReferencesComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        MatIconModule], providers: [
+        provideClientHydration(),
+        { provide: APP_BASE_HREF, useValue: '/' },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
