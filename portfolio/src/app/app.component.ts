b8781f5c21c4212820e22e124a1baa220cf52a20
RamkumarMrj <ramkumar.mrj97@gmail.com>
2026-03-27 23:20:38 +0530

    Refactor: Merge all components into AppComponent

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DataSourceService } from './services/data-source.service';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -6,7 +10,59 @@ import { Component } from '@angular/core';
     styleUrl: './app.component.css',
     standalone: false
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ramkumar Murthy';
+  data: any;
+  private iconPath: string = "assets/icons";
 
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer,
+    @Inject(APP_BASE_HREF) private baseHref: string,
+    private dataService: DataSourceService
+  ) {
+    this.registerIcons();
+  }
+
+  ngOnInit(): void {
+    this.getResume();
+  }
+
+  private registerIcons(): void {
+    // About/Background icons
+    this.iconRegistry.addSvgIcon('about', this.setPath(`${this.iconPath}/about.svg`));
+    this.iconRegistry.addSvgIcon('work', this.setPath(`${this.iconPath}/work.svg`));
+    this.iconRegistry.addSvgIcon('projects', this.setPath(`${this.iconPath}/projects.svg`));
+    this.iconRegistry.addSvgIcon('skills', this.setPath(`${this.iconPath}/skills.svg`));
+    this.iconRegistry.addSvgIcon('certificates', this.setPath(`${this.iconPath}/certificates.svg`));
+    this.iconRegistry.addSvgIcon('education', this.setPath(`${this.iconPath}/education.svg`));
+    this.iconRegistry.addSvgIcon('references', this.setPath(`${this.iconPath}/references.svg`));
+    
+    // Social media icons
+    this.iconRegistry.addSvgIcon('instagram', this.setPath(`${this.iconPath}/social-media/instagram.svg`));
+    this.iconRegistry.addSvgIcon('twitter', this.setPath(`${this.iconPath}/social-media/twitter-x.svg`));
+    this.iconRegistry.addSvgIcon('github', this.setPath(`${this.iconPath}/social-media/github.svg`));
+    this.iconRegistry.addSvgIcon('pinterest', this.setPath(`${this.iconPath}/social-media/pinterest.svg`));
+    this.iconRegistry.addSvgIcon('threads', this.setPath(`${this.iconPath}/social-media/threads.svg`));
+    this.iconRegistry.addSvgIcon('medium', this.setPath(`${this.iconPath}/social-media/medium.svg`));
+    this.iconRegistry.addSvgIcon('dev.to', this.setPath(`${this.iconPath}/social-media/dev-to.svg`));
+    this.iconRegistry.addSvgIcon('stackoverflow', this.setPath(`${this.iconPath}/social-media/stack-overflow.svg`));
+    this.iconRegistry.addSvgIcon('linkedin', this.setPath(`${this.iconPath}/social-media/linkedin.svg`));
+    this.iconRegistry.addSvgIcon('facebook', this.setPath(`${this.iconPath}/social-media/facebook.svg`));
+    
+    // Utility icons
+    this.iconRegistry.addSvgIcon('download', this.setPath(`${this.iconPath}/download.svg`));
+    this.iconRegistry.addSvgIcon('open-link', this.setPath(`${this.iconPath}/open-link.svg`));
+  }
+
+  private setPath(url: string): SafeResourceUrl {
+    // In components, the path was "../../assets/icons", here it is "assets/icons"
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.baseHref + url)
+  }
+
+  getResume() {
+    this.dataService.getData().subscribe((data) => {
+      this.data = data;
+    });
+  }
 }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,34 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { BackgroundComponent } from './components/background/background.component';
-import { WorkExperienceComponent } from './components/background/work-experience/work-experience.component';
-import { ProjectsComponent } from './components/background/projects/projects.component';
-import { SkillsComponent } from './components/background/skills/skills.component';
-import { CertificationsComponent } from './components/background/certifications/certifications.component';
-import { EducationComponent } from './components/background/education/education.component';
-import { ReferencesComponent } from './components/background/references/references.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { APP_BASE_HREF } from '@angular/common';
 
-@NgModule({ declarations: [
-        AppComponent,
-        ProfileComponent,
-        BackgroundComponent,
-        WorkExperienceComponent,
-        ProjectsComponent,
-        SkillsComponent,
-        CertificationsComponent,
-        EducationComponent,
-        ReferencesComponent,
+@NgModule({
+    declarations: [
+        AppComponent
     ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
         AppRoutingModule,
-        MatIconModule], providers: [
+        MatIconModule
+    ],
+    providers: [
         provideClientHydration(),
         { provide: APP_BASE_HREF, useValue: '/' },
         provideHttpClient(withInterceptorsFromDi())
-    ] })
+    ]
+})
 export class AppModule { }
