77361d1fa64fa764f2f47d6f252a21b4b45b4d45
RamkumarMrj <ramkumar.mrj97@gmail.com>
2026-03-29 00:07:46 +0530

    chore: update data source and image assets to remote GitHub URLs, increased hero background visibility

--- portfolio/src/environments/environment.development.ts
+++ portfolio/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // baseUrl: 'https://gist.githubusercontent.com/RamkumarMrj/aef0357b69bd4cb6bd25b8875a815e52/raw/6d318616abd2998983f3ab5d60ff3bf13557674e/resume.json'
-    baseUrl: '../../assets/data/resume.json'
+    baseUrl: 'https://raw.githubusercontent.com/RamkumarMrj/RamkumarMrj/master/portfolio/src/assets/data/resume.json'
 };
